5297abbb4fc61ca5dc391f08ecd5bcd63a08e841
Monine-company <bob.zhoxiang@gmail.com>
2016-12-07 14:06:49 +0800

    tool function for javascript

---
+++ JS-ToolClassFun.js
@@ -0,0 +1,27 @@
+/**
+ * getQueryString() 获取 URL querystring 中的参数
+ *   [EN] => getQueryString() get param from URL querystring
+ *
+ * @param {String} name (如不传则返回所有查询对象)
+ *   [EN] => @param {String} name (return all query objects when not has value)
+ *
+ * @return {Null || String || Object} 
+ */
+var getQueryString = function(name) {
+  var querystring = location.search;
+  var param = {};
+  var kvs = null;
+  var kv = null;
+
+  if (!querystring) return null;
+
+  querystring = querystring.replace(/^\?/, '');
+  kvs = querystring.split('&');
+
+  kvs.forEach(function(elem, index, arr) {
+    kv = elem.split('=');
+    param[kv[0]] = decodeURIComponent(kv[1]);
+  });
+
+  return (name ? param[name] : param);
+};
