380e17101f15626f386e5e74b6e1e141384ed73d
monine <bob.zhouxiang@gmail.com>
2017-04-17 23:50:25 +0800

    webpack demo

---
+++ webpack-demo/app/index.js
@@ -0,0 +1,17 @@
+var _ = require('lodash');
+var moment = require('moment');
+
+require('./index.css');
+
+function component() {
+  var element = document.createElement('div');
+  var m = moment;
+  console.log(m);
+
+  /* 需要引入 lodash，下一行才能正常工作 */
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack-demo/build/webpack.base.conf.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var config = require('../config');
+var utils = require('./utils');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: {
+    main: './app/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: config.build.assetsRoot,
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }],
+  }
+};
